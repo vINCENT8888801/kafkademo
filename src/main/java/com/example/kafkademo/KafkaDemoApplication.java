8d6326f7d8b75445abd05e419eec92f3e6084426
Wei Seng <tanvincent999@gmail.com>
2025-06-08 16:44:33 +0800

    remove unused import

--- src/main/java/com/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -3,9 +3,6 @@ package com.example.kafkademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class KafkaDemoApplication {
 	public static void main(String[] args) {
